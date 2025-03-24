f947b93355180b28842eb23566009c4a5ecd30d1
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-03-24 16:44:47 -0300

    git igonere

---
+++ TODO/Controllers/BoardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TODO.Controllers
+{
+    public class BoardsController : Controller
+    {
+        public IActionResult Boards()
+        {
+            return View();
+        }
+    }
+}
